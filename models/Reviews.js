f80cf8809293f1cf336d1d62600f204ebbacc2c8
austinp <auspendleton@gmail.com>
2024-08-16 08:52:18 -0500

    Review model added

---
+++ models/Reviews.js
@@ -0,0 +1,36 @@
+// models/Review.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  hotelName: {
+    type: String,
+    required: true,
+  },
+  reviewText: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
