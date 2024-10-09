f04a41ee1b7344a5517b23bf7c203a7c5f4484d1
austinp <auspendleton@gmail.com>
2024-10-09 16:35:39 -0600

    user controller updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,45 +1,22 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-exports.registerUser = async (req, res) => {
+const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
     let user = await User.findOne({ email });
-
     if (user) {
       return res.status(400).json({ msg: "User already exists" });
     }
 
-    user = new User({
-      name,
-      email,
-      password,
-    });
-
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
-
+    user = new User({ name, email, password });
     await user.save();
 
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
+    res.status(201).json({ msg: "User registered successfully" });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
   }
 };
+
+module.exports = { registerUser };
